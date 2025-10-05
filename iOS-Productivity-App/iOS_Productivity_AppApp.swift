c0946d93b4823d9df9c375a4bf084f04cec31e49
Adib Hasan <adib@Adibs-MacBook-Air.local>
2025-10-05 22:32:18 +0600

    Initial commit: SwiftUI project setup

---
+++ iOS-Productivity-App/iOS_Productivity_AppApp.swift
@@ -0,0 +1,19 @@
+//
+//  iOS_Productivity_AppApp.swift
+//  iOS-Productivity-App
+//
+//  Created on 2025-10-05.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Productivity_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+// TODO: Firebase configuration will be added in Story 1.2
